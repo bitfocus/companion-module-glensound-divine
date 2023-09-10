5e0cc639e21084df469f3aa5d1dca9989c99da54
Peter Daniel <peterdaniel@peters-macbook-air.local>
2023-09-10 16:13:54 +0100

    preset

---
+++ presets.js
@@ -0,0 +1,43 @@
+import { combineRgb } from '@companion-module/base'
+
+export function updatePresets() {
+	let presets = {}
+
+	presets['VolumeKnob'] = {
+		type: 'button',
+		category: 'Volume',
+		name: 'Volume Knob',
+		options: { 
+			rotaryActions: true 
+		},
+		style: {
+			text: 'Volume',
+			size: '18',
+			color: combineRgb(255, 255, 255),
+			bgcolor: combineRgb(0, 0, 0),
+		},
+		steps: [
+			{
+				rotate_left: [
+					{
+						actionId: 'dec_volume',
+						options: {
+							step_down: 4,
+						},
+					},
+				],
+				rotate_right: [
+					{
+						actionId: 'inc_volume',
+						options: {
+							step_up: 4,
+						},
+					},
+				],
+			},
+		],
+		feedbacks: [],
+	}
+
+	this.setPresetDefinitions(presets)
+}
